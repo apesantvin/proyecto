34152b371c9d11f17178aeec44607c472efe30d6
Joel <apalexisjoel@gmail.com>
2020-01-07 04:01:00 +0100

    Optimizado el sistema de actualizacion de la tabla de las gráficas

--- GestionCO2/views.py
+++ GestionCO2/views.py
@@ -43,12 +43,6 @@ def empresa_lista(request):
 
 def empresa_detalles(request,pk):
     empresa = get_object_or_404(Empresa, pk=pk)
-    min_year=calcular_year_minimo(empresa)
-    if min_year==0:
-        datos=0
-        min_year=timezone.now().year
-    else:
-        datos=1
     consumos=Factores_conversion.objects.first()
     co2_anual=CO2_Empresa_Año.objects.filter(empresa=empresa,Graf='1')
     co2_edificios=CO2_Empresa_Año.objects.filter(empresa=empresa,Graf='2')
@@ -74,22 +68,23 @@ def empresa_detalles(request,pk):
     crear_gráficas(co2_edificios,'2')
     crear_gráficas(co2_viajes,'3')
     crear_gráficas(co2_vehiculos,'4')
-    return render(request, 'GestionCO2/empresa_detalles_principales.html', {'empresa': empresa, 'anual':co2_anual, 'edificios':co2_edificios, 'vehiculos':co2_vehiculos, 'viajes':co2_viajes, 'datos':datos})
+    return render(request, 'GestionCO2/empresa_detalles_principales.html', {'empresa': empresa, 'edificios':co2_edificios, 'vehiculos':co2_vehiculos, 'viajes':co2_viajes, 'datos':1})
 
 @login_required
 def actualizar_datos(request,pk):
     empresa = get_object_or_404(Empresa, pk=pk)
-    min_year=calcular_year_minimo(empresa)
-    if min_year==0:
-        datos=0
-        min_year=timezone.now().year
-    else:
-        datos=1
     consumos=Factores_conversion.objects.first()
-    co2_anual=calcular_CO2_year(pk, min_year,consumos)
-    co2_edificios=calcular_CO2_edificios(pk, min_year,consumos)
-    co2_vehiculos=calcular_CO2_vehiculos(pk, min_year,consumos)
-    co2_viajes=calcular_CO2_viajes(pk, min_year)
+    co2_edificios=calcular_consumo_Edificio(empresa,consumos)
+    co2_vehiculos=calcular_consumo_vehiculos(empresa,consumos)
+    co2_viajes=calcular_consumo_viajes(empresa,consumos)
+    co2_anual=[]
+    for datos in co2_edificios:
+        co2_anual.append(datos)
+    for datos in co2_vehiculos:
+        co2_anual.append(datos)
+    for datos in co2_viajes:
+        co2_anual.append(datos)
+    co2_anual=ordenar_agrupar(co2_anual)
     TotalAños=CO2_Empresa_Año.objects.filter(empresa=empresa,Graf='1')
     for dato in TotalAños:
         dato.delete()
@@ -114,7 +109,8 @@ def actualizar_datos(request,pk):
     for dato in co2_vehiculos:
         CO2_Empresa_Año.objects.create(empresa=empresa,Año=dato[0],CO2_generado=float(dato[1]),Graf='4')
 
-    return redirect('empresa_detalles', pk=empresa.pk)    
+    #return redirect('empresa_detalles', pk=empresa.pk)    
+    return render(request, 'GestionCO2/empresa_detalles_principales.html', {'empresa': empresa,'anual':co2_anual, 'edificios':co2_edificios, 'vehiculos':co2_vehiculos, 'viajes':co2_viajes, 'datos':1})
 
 @login_required
 def empresa_configuracion(request, pk):
@@ -219,7 +215,7 @@ def empresa_configuracion(request, pk):
                             else:
                                 messages.error(request, f'El tipo de consumo que  usted ha generado en la linea {line_count + 1} no existe en nuestra base de datos')
                         elif row[0] == 'Viaje':
-                            if row[6] in tipo_transporte:
+                            if row[5] in tipo_transporte:
                                 personales=Personal.objects.filter(empresa=e)
                                 nombre_personales=[]
                                 for personal in personales:
@@ -551,199 +547,79 @@ def mensajes_todos_experto(request):
     if no_experto==1:
         return render(request, 'experto/base_base_experto.html', {'experto':no_experto})
 
-#Vistas CO2 generado
-#CO2 generado por los edificios de una empresa en un año determinado
-def edificio_year(empresa, year, consumo):
-    edificios_consumos=[]
-    fecha_edificio='fecha_adquisicion'
-    edificios=Edificio.objects.filter(empresa=empresa).order_by(fecha_edificio)
-    consumo_edificios=0;
-    for edificio in edificios:
-        fecha=edificio.fecha_adquisicion.year
-        if (fecha <= year):
-            consumo_edificios=consumo_edificios+edificio_consumo(edificio, year, consumo)
-    return consumo_edificios
-
-#Co2 generado en un edificio y un año                
-def edificio_consumo(edificio, year, consumo):
-    agua=edificio_consumo_tipo(edificio, year, consumo.Edificio_consumo_Agua,'1')
-    electricidad=edificio_consumo_elec(edificio, year, consumo.Edificio_consumo_Electricidad,'2')
-    aceite=edificio_consumo_tipo(edificio, year, consumo.Edificio_consumo_Aceite,'3')
-    propano=edificio_consumo_tipo(edificio, year, consumo.Edificio_consumo_Propano,'4')
-    gasNatural=edificio_consumo_tipo(edificio, year, consumo.Edificio_consumo_GasNatural,'5')
-    return agua+aceite+electricidad+propano+gasNatural
-
-#Co2 generado por un consumo en un edificio y un año
-def edificio_consumo_tipo(edificio, year, consumo, tipo):
-    total_consumo=0
-    consumos=EdificioConsumo.objects.filter(edificio=edificio,tipo=tipo)
-    for c in consumos:
-        fecha=c.fecha_consumo.year
-        if (fecha==year):
-            total_consumo=total_consumo+(float(c.cantidad_consumida)*float(consumo))
-    return total_consumo
-
-def edificio_consumo_elec(edificio, year, consumo, tipo):
-    total_consumo=0;
-    consumos=EdificioConsumo.objects.filter(edificio=edificio,tipo=tipo).order_by('fecha_consumo')
-    fecha_ant=date(year-1,12,31)
-    fecha=date(year,1,1)
-    generado=0
-    indice=0
-    huella=0
-    while fecha.year<year+1 and indice<len(consumos):
-        if fecha_ant<fecha:
-            generado=generado+calcular_energiaGenerada(edificio, fecha)
-        if consumos[indice].fecha_consumo==fecha:
-            co2=float(consumos[indice].cantidad_consumida)-generado
-            fecha_ant=fecha
-            indice=indice+1
-            if co2<=0:
-                generado=abs(co2)
-                huella = huella+0
-            else:
-                huella=huella+co2*float(consumo)
-        elif consumos[indice].fecha_consumo<fecha:
-            fecha_ant=fecha
-            indice=indice+1
+def ordenar_agrupar(lista):
+    lista.sort()
+    nuevamatriz=[]
+    nuevamatriz.append([])
+    i=0
+    nuevamatriz[0].append(lista[0][0])
+    nuevamatriz[0].append(0)
+    for dato in lista:
+        if dato[0]==nuevamatriz[i][0]:
+            nuevamatriz[i][1]=nuevamatriz[i][1]+dato[1]
         else:
-            fecha=fecha+datetime.timedelta(days=1)
-    return huella
+            nuevamatriz.append([])
+            i=i+1
+            nuevamatriz[i].append(dato[0])
+            nuevamatriz[i].append(dato[1])
+    return nuevamatriz
 
-#Energía generada de por generadores (no genera co2)
-def calcular_energiaGenerada(edificio, fecha):
-    generado=0
-    generadores=Generador.objects.filter(edificio=edificio, fecha_generacion=fecha)
-    for generador in generadores:
-        generado=generado+float(generador.cantidad_generada)
-    return generado           
-
-#CO2 generado por los vehiculos de una empresa en año determinado
-def vehiculo_year(empresa, year, consumo):
-    fecha_vehiculo='fecha_compra'
-    vehiculos=Vehiculo.objects.filter(empresa=empresa).order_by(fecha_vehiculo)
-    consumo_vehiculo=0;
-    for vehiculo in vehiculos:
-        fecha=vehiculo.fecha_compra.year
-        if (fecha <= year):
-            consumo_vehiculo=consumo_vehiculo+vehiculo_consumo(vehiculo, year, consumo)
-    return consumo_vehiculo
-
-#Co2 generado en un vehiculo y un año                
-def vehiculo_consumo(vehiculo, year, consumo):
-    electricidad=vehiculo_consumo_tipo(vehiculo, year, consumo.Vehiculo_consumo_Electricidad,'1')
-    gasolina=vehiculo_consumo_tipo(vehiculo, year, consumo.Vehiculo_consumo_Gasolina,'2')
-    diesel=vehiculo_consumo_tipo(vehiculo, year, consumo.Vehiculo_consumo_Diesel,'3')
-    return electricidad+gasolina+diesel
-
-#Co2 generado por un consumo en un vehiculo y un año
-def vehiculo_consumo_tipo(vehiculo, year, consumo, tipo):
-    total_consumo=0;
-    consumos=VehiculoConsumo.objects.filter(vehiculo=vehiculo,tipo=tipo)
-    for c in consumos:
-        fecha=c.fecha_consumo.year
-        if (fecha==year):
-            total_consumo=total_consumo+(float(c.cantidad_consumida)*float(consumo))
-    return total_consumo
-
-#CO2 generado por los viajes de una empresa en año determinado
-def viaje_year(empresa, year):
-    co2_total=0;
-    viajes=Viaje.objects.all()
-    for viaje in viajes:
-        fecha=viaje.fecha_viaje.year
-        if (fecha==year):
-            v='ok'
-            personas=viaje.personal.all()
-            for p in personas:
-                if p.empresa != empresa:
-                    v='not'
-            if v=='ok':
-                co2_total=co2_total+float(viaje.co2)
-    return co2_total
-
-#Calcular CO2 generado por la empresa en todos los años
-def calcular_CO2_year(pk, min_year, consumos):
-    empresa=get_object_or_404(Empresa, pk=pk)
-    today_year=date.today().year
-    matriz_co2=[]
-    for year in range(min_year, today_year+1):
-        co2_year=[]
-        co2_year.append(year)
-        edificio=edificio_year(empresa, year, consumos)
-        vehiculo=vehiculo_year(empresa, year, consumos)
-        viaje=viaje_year(empresa, year)
-        co2_year.append(edificio+vehiculo+viaje)
-        matriz_co2.append(co2_year)
-    return matriz_co2
-
-#Calcular CO2 generado por los edificios de la empresa en todos los años
-def calcular_CO2_edificios(pk, min_year, consumos):
-    empresa=get_object_or_404(Empresa, pk=pk)
-    today_year=date.today().year
-    matriz_co2=[]
-    for year in range(min_year, today_year+1):
-        co2_edificio=[]
-        co2_edificio.append(year)
-        co2_edificio.append(edificio_year(empresa, year, consumos))
-        matriz_co2.append(co2_edificio)
-    return matriz_co2
-
-#Calcular CO2 generado por los vehiculos de la empresa en todos los años
-def calcular_CO2_vehiculos(pk, min_year, consumos):
-    empresa=get_object_or_404(Empresa, pk=pk)
-    today_year=date.today().year
-    matriz_co2=[]
-    for year in range(min_year, today_year+1):
-        co2_vehiculos=[]
-        co2_vehiculos.append(year)
-        co2_vehiculos.append(vehiculo_year(empresa, year, consumos))
-        matriz_co2.append(co2_vehiculos)
-    return matriz_co2
-
-#Calcular CO2 generado por los viajes de la empresa en todos los años
-def calcular_CO2_viajes(pk, min_year):
-    empresa=get_object_or_404(Empresa, pk=pk)
-    today_year=date.today().year
-    matriz_co2=[]
-    for year in range(min_year, today_year+1):
-        co2_viajes=[]
-        co2_viajes.append(year)
-        co2_viajes.append(viaje_year(empresa, year))
-        matriz_co2.append(co2_viajes)
-    return matriz_co2
-
-#Calculo del menor año del que ce conoce consumo
-def calcular_year_minimo(empresa):
-    fecha_consumo='fecha_consumo'
+def calcular_consumo_Edificio(empresa,factores):
     edificios=Edificio.objects.filter(empresa=empresa)
-    vehiculos=Vehiculo.objects.filter(empresa=empresa)
-    personal=Personal.objects.filter(empresa=empresa)
-    viajes=Viaje.objects.all()
-    fecha_edificio=timezone.now().date()
-    fecha_vehiculo=timezone.now().date()
-    fecha_persona=timezone.now().date()
-    ed=0
-    ve=0
-    vi=0
+    matriz=[]
+    i=0
     for edificio in edificios:
-        consumo_edificio=EdificioConsumo.objects.filter(edificio=edificio).order_by('fecha_consumo')
-        if consumo_edificio.count() != 0:
-            fecha_edificio=min(fecha_edificio, consumo_edificio[0].fecha_consumo)
-            ed=ed+1
+        consumos=EdificioConsumo.objects.filter(edificio=edificio)
+        for consumo in consumos:
+            matriz.append([])
+            matriz[i].append(consumo.fecha_consumo.year)
+            factor=0
+            if consumo.tipo=='1' or consumo.tipo=='Agua' :
+                factor=float(factores.Edificio_consumo_Agua)
+            elif consumo.tipo=='2' or consumo.tipo=='Electricidad' :
+                factor=float(factores.Edificio_consumo_Electricidad)
+            elif consumo.tipo=='3' or consumo.tipo=='Aceite' :
+                factor=float(factores.Edificio_consumo_Aceite)
+            elif consumo.tipo=='4' or consumo.tipo=='Propano':
+                factor=float(factores.Edificio_consumo_Propano)
+            elif consumo.tipo=='5' or consumo.tipo=='Gas Natural' :
+                factor=float(factores.Edificio_consumo_GasNatural)
+            matriz[i].append(float(consumo.cantidad_consumida)*factor)
+            i=i+1
+    nuevamatriz=ordenar_agrupar(matriz)
+    return nuevamatriz
+
+def calcular_consumo_vehiculos(empresa,factores):
+    vehiculos=Vehiculo.objects.filter(empresa=empresa)
+    matriz=[]
+    i=0
     for vehiculo in vehiculos:
-        consumo_vehiculo=VehiculoConsumo.objects.filter(vehiculo=vehiculo).order_by('fecha_consumo')
-        if consumo_vehiculo.count() != 0:
-            fecha_vehiculo=min(fecha_vehiculo, consumo_vehiculo[0].fecha_consumo)
-            vi=vi+1
+        consumos=VehiculoConsumo.objects.filter(vehiculo=vehiculo)
+        for consumo in consumos:
+            matriz.append([])
+            matriz[i].append(consumo.fecha_consumo.year)
+            factor=0
+            if consumo.tipo=='1' or consumo.tipo=='Electricidad' :
+                factor=float(factores.Vehiculo_consumo_Electricidad)
+            elif consumo.tipo=='2' or consumo.tipo=='Gasolina' :
+                factor=float(factores.Vehiculo_consumo_Gasolina)
+            elif consumo.tipo=='3' or consumo.tipo=='Diesel':
+                factor=float(factores.Vehiculo_consumo_Diesel)
+            matriz[i].append(float(consumo.cantidad_consumida)*factor)
+            i=i+1
+    nuevamatriz=ordenar_agrupar(matriz)
+    return nuevamatriz
+
+def calcular_consumo_viajes(empresa,factores):
+    viajes=Viaje.objects.all()
+    matriz=[]
+    i=0
     for viaje in viajes:
-        personas=viaje.personal.all()
-        for persona in personas:
-            if persona.empresa==empresa:
-                fecha_persona=min(fecha_persona,viaje.fecha_viaje)
-                ve=ve+1
-    fecha_minima=min(fecha_persona,fecha_vehiculo,fecha_edificio)
-    if ed==0 and ve==0 and vi==0:
-        return 0
-    else:
-        return fecha_minima.year
+        pers = viaje.personal.all()
+        if pers[0].empresa==empresa:
+            matriz.append([])
+            matriz[i].append(viaje.fecha_viaje.year)
+            matriz[i].append(viaje.co2+viaje.noches_hotel*float(factores.Viaje_consumo_noches))
+            i=i+1
+    nuevamatriz=ordenar_agrupar(matriz)
+    return nuevamatriz
